e468aa0a0f20a2e2a2d4948f441705b0ce0fb670
Umer-FarooqAhmed <123248058+Umer-Farooq-Ahmed@users.noreply.github.com>
2023-02-04 01:16:31 +0500

    add.js
    add.js

---
+++ app.js
@@ -0,0 +1,280 @@
+// ***** CHAPTER NO#5
+
+// Question no1
+// var num1 =(3)
+// var num2 =(5)
+// var total = num1 + num2
+
+// console.log (total)
+
+// Question no2(i)
+// var num1 =(3)
+// var num2 =(5)
+// var total = num1 - num2
+
+// console.log (total)
+
+// //  Question no2(ii)
+
+// var num1 =(3)
+// var num2 =(5)
+// var total = num1 * num2
+
+// console.log (total)
+
+// //  Question no2(iii)
+
+// var num1 =(3)
+// var num2 =(5)
+// var total = num1 / num2
+
+// console.log (total)
+
+//  Question no2(vi)
+
+// var num1 =(3)
+// var num2 =(5)
+// var total = num1 % num2
+
+// console.log (total)
+
+// Question no3(i)
+
+// var num
+
+
+// // Question no3(ii)
+
+//  var num =(4)
+
+//  console.log (num)
+
+// // Question no3(iii)
+
+// var num =
+
+// console.log (num)
+
+// //  Question no3(iv)
+
+// var num =5
+
+// console.log (num)
+
+//  Question no3(v)
+
+// var num1 =3
+// var num2 =2
+
+// console.log (num1 + num2)
+
+// //  Question no3(vi)
+
+// var num1 =5
+// var num2 =1
+
+// console.log (num1 + num2)
+
+// //  Question no3(vii)
+
+// var num1 =5
+// var num2 =1
+// var num3 =7
+
+// console.log (num1 + num2 + num3)
+
+// //  Question no3(viii)
+
+// var num1 =5
+// var num3 =7
+
+// console.log (num1 + num3)
+
+// //  Question no3(ix)
+
+// var num1 =5
+// var num3 =7
+
+// console.log (num1 + num3)
+
+// //  Question no3(x)
+
+// var num1 =9
+// var num3 =3
+
+// console.log (num1 / num3)
+
+// //  Question no3(xi)
+
+// var num1 =9
+// var num3 =3
+
+// console.log (num1 % num3)
+
+// //  Question no4
+
+// var moivePrice ="600"
+
+// console.log( moivePrice * 5)
+
+// // //  Question no5
+
+// var table =(4)
+
+// console.log(table * 1)
+// console.log(table * 2)
+// console.log(table * 3)
+// console.log(table * 4)
+// console.log(table * 5)
+// console.log(table * 6)
+// console.log(table * 7)
+// console.log(table * 8)
+// console.log(table * 9)
+// console.log(table * 10)
+
+// //  Question no6
+
+// var temperature ="(13'c)"
+
+// console.log( temperature)
+
+//  Question no7
+
+// document.write ("<h1>" +'Shopping card' + "</h1>")
+// var item1 =("<h1>" + "price of item 1 is 650" + "</h1>")
+// var item2 =("<h1>" + "price of item 2 is 100" + "</h1>")
+// var itemCharges1 =("<h1>" + "Ordered Quantity of item 1 is 3 " + "</h1>")
+// var itemCharges2 =("<h1>" + "Ordered Quantity of item 2 is 7" + "</h1>")
+// var charges =("<h1>" +"shopping charges 100" + "</h1>")
+
+// document.write(item1)
+// document.write(itemCharges1)
+// document.write(item2)
+// document.write(itemCharges2)
+// document.write(charges)
+
+// // Question no8
+
+// document.write ("<h1>" +'Marks Sheet' + "</h1>")
+// var num2=+prompt ("Enter Total marks")
+// var num1=+prompt ("Marks obtained")
+
+// alert(num1 / num2 * 100)
+
+// //  Question no9
+
+// document.write ("<h1>" +'Currency in PKR' + "</h1>")
+// var Currency =+prompt("exchange dollar","")
+// var dollar=("270")
+// var riyal=("70")
+
+// document.write("$"+Currency * dollar)
+//  Quesion no10
+// var num1=(12)
+// var num2=(15)
+// var num3=(18)
+
+// console.log(num1 + num2)
+// console.log(num1 - num2)
+// console.log(num1 * num2)
+// console.log(num1 / num2)
+
+// //  Quesion no11
+
+//  document.write ("<h1>" +'Age calculator' + "</h1>")
+//  var birthyear =+prompt("birth year")
+//  var currentyear =+prompt("current year")
+//  alert(birthyear - currentyear+"years")
+
+
+// // //  Quesion no12
+// document.write ("<h1>" +'The Geometrizer:' + "</h1>")
+// console.log("Hint : Circumference of a circle = 2 π r , π = 3.142")
+// console.log("Hint : Area of a circle = π r2, π = 3.142")
+
+// // Quesion no13
+// document.write ("<h1>" +'The Lifetime Supply Calculator' + "</h1>")
+// var Favoritesnack=+prompt (Favorite snack)
+// var Currentage=+prompt (Current age)
+// var Maximumage=+prompt (Maximum age)
+// var Amountofsnacks=+prompt (Amount of snacks per day;)
+// var Needchocolatechips=+prompt (you will need 150 chocolate chips to last you until the ripe old age of;)
+// document.write(Favoritesnack)
+// document.write(Currentage)
+// document.write(Maximumage)
+// document.write(Amountofsnacks)
+// document.write(Needchocolatechips)
+
+// *****Chapter no6to9
+
+// //  Question no1(a)
+// var num1=("10")
+// console.log(num1)
+
+// //  Question no(b)
+// var a=("11")
+// console.log(a++)
+
+// //  Question no(c)
+// var a=("11")
+// console.log(--a)
+
+// //  Question no(d)
+// var a=("11")
+// console.log(a--)
+
+// //  Question no2(a)
+// var num1=a=2,b=1
+// var num2 =--a 
+// alert(num1 + num2)
+
+//  Question no2(b)
+// var num1=a=2,b=1
+// var num2 =--a + --b
+// alert(num1 + num2)
+
+//  Question no2(c)
+// var num1=a=2,b=1
+// var num2 =--a + --b + ++b
+// alert(num1 + num2)
+
+//  Question no2(d)
+// var num1=a=2,b=1
+// var num2 =--a + --b + ++b + b--
+// alert(num1 + num2)
+
+// // Question no3
+// document.write("Muhammad Farooq")
+
+// //  Question no5
+
+// var num1 =+prompt("default number","5")
+
+//  question no6
+
+// document.write ("<h1>" +'Subject Total Marks Obtained Marks Percentage' + "</h1>")
+// var Subject1=("English")
+// var Subject2=("Math")
+// var Subject3=("Urdu")
+
+// var TMarks=("100")
+
+// var OMarks1=("54")
+// var OMarks2=("54")
+// var OMarks3=("48")
+
+// var STotal=(300)
+// var OTotal=(156)
+// var PTotal=("52%")
+
+// document.write(Subject1 + "             " + OMarks1 + "54%")
+
+// document.write(Subject2 + "             " + OMarks2 + "54%")
+
+// document.write(Subject3 + "             " + OMarks3 + "48%")
+
+// document.write(STotal)
+
+// document.write(OTotal)
+
+// document.write(PTotal)
